af73a9801d39e68cbc4998ef376d4b3434fc272d
Jeffery <1987458665@qq.com>
2025-01-21 22:53:41 +0800

    feat: add shadcn ui

---
+++ src/const/branding.ts
@@ -0,0 +1,12 @@
+
+
+export const BRANDING_NAME = 'AI Offer Miner';
+export const BRANDING_LOGO_URL = '';
+
+export const ORG_NAME = 'Offer Miner';
+
+export const BRANDING_URL = {
+  help: undefined,
+  privacy: undefined,
+  terms: undefined,
+};
